9a2181123388595fd6f84a075712b9a19b87fa44
Jay <jay.sanjay.karia@gmail.com>
2022-05-12 22:13:48 +0530

    Update script.js

--- Cicket Player Stats/src/script.js
+++ Cicket Player Stats/src/script.js
@@ -6,6 +6,9 @@ let battingFours = document.querySelector('.fours').value;
 let battingSixes = document.querySelector('.sixes').value;
 let strikeRate = document.querySelector('.sr');
 
+let result = document.querySelector('.result').innerHTML;
+let save = document.querySelector('.save');
+
 battingBallsPlayed =  Number(battingBallsPlayed, 10);
 battingRuns =  Number(battingRuns, 10);
 strikeRate.value =  Number(strikeRate, 1000);
@@ -81,9 +84,143 @@ clear.addEventListener('click', ()=> {
     sixTextBox.value = "";
     battingSixes = 0;
     strikeRate.value = "";
+    result = "";
+    document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '0';
+    document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '0';
+    document.querySelector('.heading').getElementsByTagName('p')[0].style.background = '0)';
+    document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "0";
+    document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
 });
 
 calc.addEventListener('click', ()=> {
     strikeRate.value = battingRuns/battingBallsPlayed*100;
-    console.log(strikeRate);
+    if (strikeRate.value >= 200 || battingFours >= 5 || battingSixes >= 4) {
+        result = "Excellent";
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '190px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '50px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.background = 'rgb(118, 255, 118)';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "30px";
+        document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
+    } else if (strikeRate.value >= 150 || battingFours >= 3 || battingSixes >= 3) {
+        result = "Very Good";
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '190px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '50px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.background = 'rgb(255, 255, 2)';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "30px";
+        document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
+    } else if (strikeRate.value >= 100 || battingFours >= 2 || battingSixes >= 2) {
+        result = "Good";
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '190px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '50px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.background = 'rgb(255, 255, 118)';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "30px";
+        document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
+    }  else if (strikeRate.value < 100 || strikeRate.value >=60 || battingFours >= 1 || battingSixes >= 0) {
+        result = "Poor";
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '190px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '50px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.background = 'rgb(252, 127, 3)';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "30px";
+        document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
+    }   else if (strikeRate.value < 60 || battingFours >= 1 || battingSixes >= 0) {
+        result = "Bad";
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '190px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '50px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.background = 'rgb(166, 47, 0)';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "30px";
+        document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
+    }    else if (strikeRate.value < 30 || battingFours >= 0 || battingSixes >= 0) {
+        result = "Very Bad";
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '190px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '50px';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.background = 'rgb(166, 0, 0)';
+        document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "30px";
+        document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
+    }
+});
+
+// Bowling Stats
+let bowlingPlayerName = document.querySelector('.bolwPlayerName').value;
+let bowlingBalls = document.querySelector('.bowlBalls').value;
+let bowlingRunsGiven = document.querySelector('.bowlRuns').value;
+let bowlingEconomy = document.querySelector('.economy');
+let bowlingWickets = document.querySelector('.wickets').value;
+let bowlingHatrick = document.querySelector('.hatrick').value;
+
+bowlingBalls = Number(bowlingBalls, 10);
+bowlingRunsGiven = Number(bowlingRunsGiven, 10);
+bowlingWickets = Number(bowlingWickets, 10);
+bowlingHatrick = Number(bowlingHatrick, 10);
+bowlingRunsGiven = Number(bowlingRunsGiven, 10);
+bowlingEconomy = Number(bowlingBalls, 100);
+
+let PlayerTextBox = document.querySelector('.bolwPlayerName')
+let BallsTextBox = document.querySelector('.bowlBalls');
+let BowlingRunsTextBox = document.querySelector('.bowlRuns');
+let BowlingEconomyText = document.querySelector('.economy');
+let WicketsText = document.querySelector('.wickets');
+let hatrickText = document.querySelector('.hatrick');
+
+
+// Buttons
+let onePlusOne = document.querySelector('.onePlusOne');
+let onePlusTwo = document.querySelector('.onePlusTwo');
+let onePlusThree = document.querySelector('.onePlusThree');
+let onePlusFour = document.querySelector('.onePlusFour');
+let twoPlus = document.querySelector('.twoPlus');
+let sixPlus = document.querySelector('.sixPlus');
+let fourPlus = document.querySelector('.fourPlus');
+
+onePlusOne.addEventListener('click', ()=> {
+    bowlingBalls = bowlingBalls+1;
+    BallsTextBox.value = bowlingBalls;
+});
+
+onePlusTwo.addEventListener('click', ()=> {
+    bowlingRunsGiven = bowlingRunsGiven+1;
+    BowlingRunsTextBox.value = bowlingRunsGiven;
+});
+
+onePlusThree.addEventListener('click', ()=> {
+    bowlingWickets = bowlingWickets+1;
+    WicketsText.value = bowlingWickets;
+});
+
+onePlusFour.addEventListener('click', ()=> {
+    bowlingHatrick = bowlingHatrick+1;
+    hatrickText.value = bowlingHatrick;
+});
+
+twoPlus.addEventListener('click', ()=> {
+    bowlingRunsGiven = bowlingRunsGiven+2;
+    BowlingRunsTextBox.value = bowlingRunsGiven;
+});
+
+fourPlus.addEventListener('click', ()=> {
+    bowlingRunsGiven = bowlingRunsGiven+4;
+    BowlingRunsTextBox.value = bowlingRunsGiven;
+});
+
+sixPlus.addEventListener('click', ()=> {
+    bowlingRunsGiven = bowlingRunsGiven+6;
+    BowlingRunsTextBox.value = bowlingRunsGiven;
+});
+
+let ballClear = document.querySelector('.bowlClear');
+ballClear.addEventListener('click', ()=> {
+    BallsTextBox.value = "";
+    bowlingBalls = 0;
+    BowlingRunsTextBox.value = "";
+    bowlingRunsGiven = 0;
+    WicketsText.value = "";
+    bowlingWickets = 0;
+    hatrickText.value = "";
+    bowlingHatrick = 0;
+    bowlingEconomy.value = "";
+    result = "";
+    // document.querySelector('.heading').getElementsByTagName('p')[0].style.width = '0';
+    // document.querySelector('.heading').getElementsByTagName('p')[0].style.height = '0';
+    // document.querySelector('.heading').getElementsByTagName('p')[0].style.background = '0';
+    // document.querySelector('.heading').getElementsByTagName('p')[0].style.fontSize = "0";
+    // document.querySelector('.heading').getElementsByTagName('p')[0].innerHTML = result;
 });
